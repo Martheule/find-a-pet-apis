590449247b45324cbd13706691cc5c04afdd6aae
Paramveer <paramveermarwahafc@gmail.com>
2025-07-25 15:46:50 +0200

    added request body parser

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ import express from "express";
 // initialize the app
 export const app = express();
 
+// middleware to parse request body
+app.use(expree.json());
+
 // mini app per resource
 app.use();
 app.use();
