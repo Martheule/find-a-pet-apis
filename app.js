0cb8db8c34c0ef1dac201ce12278676e7be66fe1
Paramveer <paramveermarwahafc@gmail.com>
2025-07-30 15:43:13 +0200

    minor refactoring

--- app.js
+++ app.js
@@ -1,24 +1,19 @@
-import express from 'express';
-import userRouter from './routes/userRouter.js';
-import categoryRouter from './routes/categoryRouter.js';
-import errorHandler from './middlewares/errorHandler.js';
+import express from "express";
+import userRouter from "./routes/userRouter.js";
+import categoryRouter from "./routes/categoryRouter.js";
+import errorHandler from "./middlewares/errorHandler.js";
+import petRouter from "./routes/petRouter.js";
 
-// initialize the app
 export const app = express();
 
-// middleware to parse request body
 app.use(express.json());
 
-// mini app per resource
-app.use('/users', userRouter);
-app.use('/category', categoryRouter);
-// app.use();
-// app.use();
+app.use("/users", userRouter);
+app.use("/category", categoryRouter);
+app.use("/pets", petRouter);
 
-//
-// app.use((req, res) => {
-//   throw new Error('Page Not Found!', { cause: 404 });
-// });
-//
+app.use((req, res) => {
+  throw new Error("Page Not Found!", { cause: 404 });
+});
 
 app.use(errorHandler);
