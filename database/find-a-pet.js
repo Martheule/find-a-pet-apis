b22c9f3b26b77ce89854c1d8f3a50c751e325e48
Paramveer <paramveermarwahafc@gmail.com>
2025-07-30 15:31:23 +0200

    logging turned off

--- database/find-a-pet.js
+++ database/find-a-pet.js
@@ -1,5 +1,7 @@
-import { Sequelize } from 'sequelize';
+import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize(process.env.NEON_URI);
+const sequelize = new Sequelize(process.env.NEON_URI, {
+  logging: false,
+});
 
 export default sequelize;
