48af2266862f72400e8d15688da5935e55366017
Paramveer <paramveermarwahafc@gmail.com>
2025-07-30 15:46:51 +0200

    minor changes

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import { app } from "./app.js";
 import dotenv from "dotenv";
-// import { setupAssociations } from "./database/associations.js";
+import { setupAssociations } from "./database/associations.js";
 
 dotenv.config();
 
