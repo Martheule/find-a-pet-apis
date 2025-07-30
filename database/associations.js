cec438c9e7ad17f1f4baa8638c3d812ca91ca6ed
Paramveer <paramveermarwahafc@gmail.com>
2025-07-30 15:31:13 +0200

    turned off logging

--- database/associations.js
+++ database/associations.js
@@ -1,1 +1,15 @@
 // Relationships will be defined here
+
+TODO: "Discuss";
+
+// create a function (setupAssociations for ex.) and add the relationships below and EXPORT!!!!
+
+// export const setupAssociations = function () {
+//   // Pet belongs to a User (owner)
+//   User.hasMany(Pet);
+//   Pet.belongsTo(User);
+
+//   // Pet belongs to a Category
+//   Category.hasMany(Pet);
+//   Pet.belongsTo(Category);
+// };
