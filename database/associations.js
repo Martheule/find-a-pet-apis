a34bea8664226f43de7ae508551ffdb3efbec065
Jialiang (Leo) <jialiang.lai@outlook.com>
2025-07-30 19:53:03 +0200

    message added

--- database/associations.js
+++ database/associations.js
@@ -1,6 +1,7 @@
 import Category from "../models/Category.js";
 import Pet from "../models/Pet.js";
 import User from "../models/User.js";
+import Message from "../models/Message.js";
 
 export const setupAssociations = function () {
   // Pet belongs to a User (owner)
@@ -10,4 +11,13 @@ export const setupAssociations = function () {
   // Pet belongs to a Category
   Category.hasMany(Pet);
   Pet.belongsTo(Category);
+
+  // Message belongs to Users and Pet
+  User.hasMany(Message, { foreignKey: "senderId", as: "sentMessages" });
+  User.hasMany(Message, { foreignKey: "receiverId", as: "receivedMessages" });
+
+  Message.belongsTo(User, { foreignKey: "senderId", as: "sender" });
+  Message.belongsTo(User, { foreignKey: "receiverId", as: "receiver" });
+
+  Message.belongsTo(Pet, { foreignKey: "petId" });
 };
