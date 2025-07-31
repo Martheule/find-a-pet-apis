1027b209fa7bb26f90416ebb9eaf674cf619fae2
Paramveer <paramveermarwahafc@gmail.com>
2025-07-31 14:27:09 +0200

    fixed association

--- database/associations.js
+++ database/associations.js
@@ -1,6 +1,7 @@
 import Category from "../models/Category.js";
 import Pet from "../models/Pet.js";
 import User from "../models/User.js";
+import sequelize from "./find-a-pet.js";
 
 export const setupAssociations = function () {
   // Pet belongs to a User (owner)
@@ -11,3 +12,5 @@ export const setupAssociations = function () {
   Category.hasMany(Pet);
   Pet.belongsTo(Category);
 };
+
+sequelize.sync({ alter: true });
